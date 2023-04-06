cc3ab852d6ad0d38036deb51041af530408fec8c
raptorcastz <chachaamaziah@gmail.com>
2023-04-06 11:12:14 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,26 +4,23 @@
  * converts_binary_to_unit - converts a binary number to unsigned int
  * @b:the string containing the binary number
  *
- * Return:the converted number,or 0 
- *
+ * Return:the converted number,or 0
  * if there is one or more chars in string b that is not 0 or 1 or b is NULL
  */
 unsigned int binary_to_unit(const char *b)
 {
 	unsigned int chacha = 0;
-
-	if (b==NULL)
-		return(0);
-	while (*b !='\0')
+if (b == NULL)
+	return (0);
+while (*b != '\0')
 	{
-		if (*b !-'0'&& *b !='1')
-			return(0);
-		chacha <<=1;
-		chacha +=*b - '0';
-		b++
+	if (*b !- '0' && *b!= '1')
+		return (0);
+	chacha << = 1;
+	chacha += *b - '0';
+	b++
 	}
-
-	return(chacha);
+	return (chacha);
 }
 
 
