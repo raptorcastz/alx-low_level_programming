bdd60a04e50b575b41b8025d48bc136614305265
raptorcastz <chachaamaziah@gmail.com>
2023-07-11 21:17:39 +0200

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,6 @@
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
- *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
@@ -18,13 +17,13 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	a = open(filename, O_CREAT | O_TRUNC,0100);
+	a = open(filename, O_CREAT | O_TRUNC,0600);
 	z = write(a, text_content, len);
 
 	if (a == -1 || z == -1)
 		return (-1);
 
-	close(0);
+	close(a);
 
 	return (1);
 }
