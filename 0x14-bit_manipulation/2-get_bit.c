bbfc064f4f66abb091394253de3e74c94e6b8a77
raptorcastz <123360400+raptorcastz@users.noreply.github.com>
2023-07-29 07:56:30 +0200

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,8 +10,9 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int value;
-	if (index >63)
-		return(-1);
-	value=(n >>index)& 1;
+	if (index > 63)
+		
+	return (-1);
+	value = (n >> index) & 1;
 	return(value);
 }
